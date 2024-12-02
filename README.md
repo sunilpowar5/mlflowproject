df29cf4b56a2351deeaac615d8d92d8417a730e8
sunilpowar5 <sunilpowar523@gmail.com>
2024-12-02 10:22:42 +0530

    MLFlow tracking

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 ## MLFlow Experiments
+
+MLFLOW_TRACKING_URI=https://dagshub.com/sunilpowar5/mlflowproject.mlflow \
+MLFLOW_TRACKING_USERNAME=sunilpowar5 \
+MLFLOW_TRACKING_PASSWORD=c2b063889b2638d6e535f226fbbfc882dbc383c2 \
+python script.py
+
--- app.py
+++ app.py
@@ -77,6 +77,11 @@ if __name__ == "__main__":
         predictions = lr.predict(train_x)
         signature = infer_signature(train_x, predictions)
 
+        ## For Remote server only (DAGSHUB)
+
+        remote_server_uri = "https://dagshub.com/sunilpowar5/mlflowproject.mlflow"
+        mlflow.set_tracking_uri(remote_server_uri)
+
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
         # Model registry does not work with file store
